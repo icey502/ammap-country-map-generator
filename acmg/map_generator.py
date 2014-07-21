e9f0f73fc210953b49d7422595dc799bf70c2ab5
icey502 <icey502@yahoo.com>
2014-07-21 09:26:53 -0700

    removed the country list, this would be provided by the caller.

--- acmg/map_generator.py
+++ acmg/map_generator.py
@@ -3,24 +3,6 @@ import sys
 import shape_to_svg
 import svg_to_ammap
 import config
-
-# the countries I want to go after
-COUNTRIES = [
-"ZW"
-]
-COUNTRIES_FULL = [
-"AF","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BH","BD","BB","BY","BE","BZ","BJ","BM","BT",
-"BO","BA","BW","BR","BN","BG","BF","BI","KH","CM","CA","CV","KY","CF","TD","CL","CN","CO","KM","CG",
-"CD","CR","HR","CU","CY","CZ","DK","DJ","DM","DO","TL","EC","EG","SV","GQ","ER","EE","ET","FJ","FI",
-"FR","GF","GA","GE","DE","GH","GR","GL","GD","GT","GN","GW","GY","HT","HN","HK","HU","IS","IN","ID",
-"IR","IQ","IE","IL","IT","CI","JM","JP","JO","KZ","KE","KI","KW","KG","LA","LV","LB","LS","LR","LY",
-"LI","LT","LU","MO","MK","MG","MW","MY","MV","ML","MT","MH","MQ","MR","MU","MX","FM","MD","MC","MN",
-"ME","MA","MZ","MM","NA","NR","NP","NL","NZ","NI","NE","NG","KP","NO","OM","PK","PA","PG","PY","PE",
-"PH","PL","PT","PR","QA","RO","RU","RW","WS","SM","ST","SA","SN","RS","SC","SL","SG","SK","SI","SB",
-"SO","ZA","KR","ES","LK","KN","LC","VC","SD","SR","SZ","SE","CH","SY","TW","TJ","TZ","TH","BS","GM",
-"TG","TO","TT","TN","TR","TM","TV","UG","UA","AE","GB","US","UY","UZ","VU","VE","VN","EH","YE","ZM",
-"ZW"
-]
         
 def generate_single_map(countryIso2Code):
     """
