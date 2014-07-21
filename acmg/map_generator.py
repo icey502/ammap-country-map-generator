306b78a5461bc1ae72657a4d640aa611fe43ace9
icey502 <icey502@yahoo.com>
2014-07-21 09:37:54 -0700

    minor output fixing

--- acmg/map_generator.py
+++ acmg/map_generator.py
@@ -8,7 +8,7 @@ def generate_single_map(countryIso2Code):
     """
     Generates a single map using shape_to_svg and svg_to_ammap
     """
-    print "+++map_generator: generateSingleMap() generating SVG for country:", countryIso2Code
+    print "+++map_generator: generate_single_map() generating SVG for country:", countryIso2Code
     shape_to_svg.generate_svg(countryIso2Code, config.SHAPEFILE)
     svg_to_ammap.run(countryIso2Code)
     print "+++map_generator: done."
@@ -17,7 +17,7 @@ def generate_all_maps(countryIso2CodeList):
     """
     Generates a set of maps using a supplied list of ISO-3166-2 codes.
     """
-    print "+++map_generator: generateAllMaps()"
+    print "+++map_generator: generate_all_maps()"
     for country in countryIso2CodeList:
         print "+++map_generator: generating SVG for country:", country
         shape_to_svg.generate_svg(country, config.SHAPEFILE)
